dd7b7de21e4d2200fa642005460870c3e3e94273
MyPC <MyPC@DRAGONAL-JOSEPH>
2018-12-24 10:00:23 +0700

    up thử

---
+++ APIselfhost/startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Owin;
+using System.Web.Http;
+
+namespace APIselfhost
+{
+    public class startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration conf = new HttpConfiguration();
+            conf.Routes.MapHttpBatchRoute(
+                name: "DefaultAPI", 
+                routeTemplate: "api/{controller}/{action}/{id}/{name}", 
+                defaults: new
+                { controller = "student",
+                    action = "get",
+                    id = RouteParameter.Optional,
+                    name = RouteParameter.Optional });
+        }
+        appBuilder.UseWebApi(conf);
+    }
+}
